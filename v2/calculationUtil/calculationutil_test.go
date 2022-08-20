21928e9c5ae6619d9345e38f387fa7b0cad8ad64
amogh2019 <amogh1@outlook.com>
2022-08-20 18:57:20 +0530

    adding SubBFromA test

--- v2/calculationUtil/calculationutil_test.go
+++ v2/calculationUtil/calculationutil_test.go
@@ -4,6 +4,13 @@ import "testing"
 
 func TestAddTwoNum(t *testing.T) {
 	if got := AddTwoNum(2, 4); got != 6 {
-		t.Fatalf("\"add testing failed\": case %v  but got %v\n", 6, got)
+		t.Fatalf("\"add testing failed\": expected %v  but got %v\n", 6, got)
+	}
+}
+
+
+func TestSubBFromA(t *testing.T) {
+	if got := SubBFromA(6, 4); got != 2 {
+		t.Fatalf("\"SubBFromA failed\": expected %v  but got %v\n", 2, got)
 	}
 }
